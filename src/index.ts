8491aab497fd3e3a2ec6fcea9e1b72a1bbe2b336
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-07-07 17:18:41 +0200

    add fetching functionality

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,22 @@
-// @ts-nocheck
-function add(a, b) {
-    return a + b
+const url = `https://picsum.photos/v2/list?page=2&limit=100%203%20Message%20#group-1`
+
+async function fetchData(url:string){
+  try {
+    const response = await fetch(url);
+
+    // check if the request was successful
+    if (!response.ok) {
+      throw new Error(`HTTP Error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    const errMsg = error instanceof Error ? error.message : 'There was an error...';
+    console.error(errMsg);
+    // throw error;
+    return [];
+    
   }
-  
-export default {}
+}
+
